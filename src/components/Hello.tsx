578b89e56a8b1f614538499fe84098026c71882b
Kevin Alejandro Manzano Torres <kevin.manzano@sqasa.co>
2023-12-10 19:18:26 -0500

    Commit 2

---
+++ src/components/Hello.tsx
@@ -0,0 +1,8 @@
+export const Hello = () => {
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+      <img src="https://www.icegif.com/wp-content/uploads/2023/08/icegif-228.gif" alt="Hello" />
+    </div>
+  );
+};

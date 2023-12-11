1b0f3563534dc62efaa1b27f1f9b24263bf5bd0c
Kevin Alejandro Manzano Torres <kevin.manzano@sqasa.co>
2023-12-10 19:20:26 -0500

    Commit 3 -> Rama2

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -2,7 +2,7 @@ export const Hello = () => {
   return (
     <div>
       <h1>Hola Mundo</h1>
-      <img src="https://www.icegif.com/wp-content/uploads/2023/08/icegif-228.gif" alt="Hello" />
+      <img src="https://gifsec.com/wp-content/uploads/2022/09/hello-gif-40.gif" alt="Hello" />
     </div>
   );
 };
